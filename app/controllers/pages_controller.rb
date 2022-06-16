d972e4720ebaa92bf24dc69a07554a36d49bad0f
Cristina Diaz <hagaren_022@hotmail.com>
2022-06-16 19:01:58 -0400

    último commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def one #one y two están enlazados con la carpeta views.
+     #Ambos deben llamarse igual para que exista correlación.
+  end
+
+  def two
+  end
+
+  def three
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root 'pages#one'  #CRUD Crear/Leer/Actualize/Eliminar.
+                   #Esto está enlazado a http.
+                   #En estos momentos el proyecto tiene 2 puertas de acceso, pages 1 y 2.
+                   #comprobamos con rails s.
+  get 'pages/two'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'pages/three'
 end
